59c63826b3694995d36ac6abe4c06c29b9ad556a
Andrew Stitcher <astitcher@apache.org>
2019-08-14 14:01:16 -0400

    WIP: Add installation files for proton-test - Now we can just install it into a virtualenv that already has the proton binding   and run it like that x It doesn't correctly install the interop tests, and it doesn't just skip them if not found   so it currently fails if installed any way other than -e (develop)

---
+++ python/tests/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from setuptools import setup
+
+setup(name='qpid-proton-tests',
+      description='Tests for Proton (AMQP based messaging library).',
+      author='Apache Qpid',
+      author_email='users@qpid.apache.org',
+      url='http://qpid.apache.org/proton/',
+      install_requires='python-qpid-proton',
+      packages=['proton_tests'],
+      entry_points={'console_scripts' : ['proton-test = proton_tests.main:main']},
+      license="Apache Software License",
+      classifiers=["License :: OSI Approved :: Apache Software License",
+                   "Intended Audience :: Developers",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 2.6",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
+                   "Programming Language :: Python :: 3.7",
+                   "Programming Language :: Python :: 3.8"]
+)
