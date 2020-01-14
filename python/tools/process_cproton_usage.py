db42a75f2290ef33eabf30f3f99bfb94907801df
Andrew Stitcher <astitcher@apache.org>
2020-01-13 22:00:57 -0500

    WIP: More analysis of the C headers - Also correlate the C headers and the analysed Python symbols   outputting definitions for the used symbols

--- python/tools/process_cproton_usage.py
+++ python/tools/process_cproton_usage.py
@@ -30,7 +30,7 @@ def parse_usage(filename: str):
     p = ast.parse(s)
     for x in ast.walk(p):
         if isinstance(x, ast.Name):
-            if x.id.startswith('PN') or x.id.startswith('pn'):
+            if x.id.startswith('PN_') or x.id.startswith('pn_'):
                 yield (x.id, (filename, x.lineno, x.col_offset))
 
 
@@ -49,29 +49,40 @@ def parse_all(dir: str):
 def parse_include(file: str, include: str):
     return parse_file(file, use_cpp=True, cpp_args=f'-I{os.path.abspath(include)}')
 
-class FuncDefVisitor(c_ast.NodeVisitor):
-    def __init__(self, syms):
-        self.syms = syms
-        self.generator = c_generator.CGenerator()
-        super(FuncDefVisitor, self).__init__()
-
-    def visit_Decl(self, node):
-        if isinstance(node.type, c_ast.FuncDecl) and node.name in self.syms:
-            print(self.generator.visit(node))
-
 class DeclVisitor(c_ast.NodeVisitor):
     def __init__(self):
         self.decls = {}
+        self.enums = {}
+        self.typedefs = {}
         super(DeclVisitor, self).__init__()
 
     def visit_Decl(self, node):
         self.decls[node.name] = node
 
+    def visit_Enum(self, node):
+        #  Enumerators are below EnumeratorList
+        for e in node.values.enumerators:
+            self.enums[e.name] = node
+
+    def visit_Typedef(self, node):
+        self.typedefs[node.name] = node
+        if isinstance(node.type.type, c_ast.Enum):
+            for e in node.type.type.values.enumerators:
+                self.enums[e.name] = node
+
+class TypeDeclVisitor(c_ast.NodeVisitor):
+    def __init__(self):
+        self.used_types = set()
+        super(TypeDeclVisitor, self).__init__()
+
+    def visit_IdentifierType(self, node):
+        self.used_types.add(node.names[0])
+
 def parse_include_decls(file, include):
     ast = parse_include(file, include)
     v = DeclVisitor()
     v.visit(ast)
-    return v.decls
+    return v.decls, v.enums, v.typedefs
 
 def main():
     argparser = argparse.ArgumentParser(description='Parse python looking for proton "pn_" & "PN_" symbols')
@@ -91,24 +102,76 @@ def main():
         finfo.update({file: linfo})
         syms.update({sym: finfo})
 
-    syms = set(syms.keys())
+    #print(syms)
+    symset = set(syms.keys())
 
     if i:
-        decls = parse_include_decls(i, p)
+        decls, enums, typedefs = parse_include_decls(i, p)
         #print(decls)
+        #print(enums)
+        #print(typedefs)
 
         generator = c_generator.CGenerator()
-        nondecls = []
-        for s in syms:
-            try:
-                print(generator.visit(decls[s]))
-            except KeyError:
-                nondecls.append(s)
-        print(sorted(nondecls))
+        used_decls = set()
+        used_enums = set()
+        used_types = set()
+        nondecls = set()
+        printed = set()
+        force_print =  [
+            '__ssize_t',
+            '__int8_t',
+            '__int16_t',
+            '__int32_t',
+            '__int64_t',
+            '__uint8_t',
+            '__uint16_t',
+            '__uint32_t',
+            '__uint64_t',
+            'size_t',
+            'ssize_t',
+            'int8_t',
+            'int16_t',
+            'int32_t',
+            'int64_t',
+            'uint8_t',
+            'uint16_t',
+            'uint32_t',
+            'uint64_t'
+        ]
 
-        #ast = parse_include(i, p)
-        #v = FuncDefVisitor(syms)
-        #v.visit(ast)
+        for s in symset:
+            if s in decls:
+                used_decls.add(s)
+                v = TypeDeclVisitor()
+                v.visit(decls[s])
+                used_types |= v.used_types
+            elif s in enums:
+                used_enums.add(s)
+            else:
+                nondecls.add(s)
+
+        for t in force_print + sorted(used_types):
+            try:
+                tp = typedefs[t]
+                if tp not in printed:
+                    print('%s;' % generator.visit(tp))
+                    printed.add(tp)
+            except KeyError:
+                pass
+
+        for s in sorted(used_enums):
+            e = enums[s]
+            if e not in printed:
+                print('%s;' % generator.visit(e))
+                printed.add(e)
+
+        for s in sorted(used_decls):
+            print('%s;' % generator.visit(decls[s]))
+
+        print("// Couldn't find definitions for the following symbols are they #defined?")
+        for  s in sorted(nondecls):
+            print('// %s' % s)
+
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
